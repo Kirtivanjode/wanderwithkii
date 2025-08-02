25dc8d3d53e1d678a2a5805e8d9adbb31e3e35f2
Kirtivanjode <kirtivanjode23@gmail.com>
2025-08-02 11:02:04 +0530

    your commit message

---
+++ frontend/src/polyfills.ts
@@ -0,0 +1,9 @@
+/***************************************************************************************************
+ * Load `$localize` for Angular i18n support (if needed).
+ */
+import '@angular/localize/init';
+
+/***************************************************************************************************
+ * Required for Angular: zone.js
+ */
+import 'zone.js'; // Included with Angular CLI.
