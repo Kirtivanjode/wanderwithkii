75bb25f9b9f6e27221c48198c47f2f113f74ffdb
Kirtivanjode <kirtivanjode23@gmail.com>
2025-09-05 16:31:24 +0530

    your commit message

--- backend/db.js
+++ backend/db.js
@@ -2,9 +2,9 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "ki",
-  host: "dpg-d2i2b8p5pdvs73eq3g00-a.oregon-postgres.render.com",
-  database: "kirti_5hp8",
-  password: "9yW1OsqTBTYtlxCqtxrnshr5OawCP7RW",
+  host: "dpg-d2ta8pvfte5s73a1l4cg-a.oregon-postgres.render.com",
+  database: "wanderwithki",
+  password: "mFz8W9To8E0UF32D44lJ3w3q36HueatO",
   port: 5432,
   ssl: {
     rejectUnauthorized: false,
